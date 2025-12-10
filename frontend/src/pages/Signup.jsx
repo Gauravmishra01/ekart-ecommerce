3df974061edffe2bc19030d46eee40819b244a9c
Gaurav Mishra <gauravmishra92812@gmail.com>
2025-12-10 10:24:46 +0530

    signup_api_correct

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -42,7 +42,7 @@ const Signup = () => {
     try {
       setLoading(true);
       const res = await axios.post(
-        `http://localhost:8000/api/v1/user/register`,
+        `${import.meta.env.VITE_API_URL}api/v1/user/register`,
         formData,
         {
           headers: {
